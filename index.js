56ec60106856b2da95aacc4ddc66240d0f2e83b7
Clive Chan <cc@clive.io>
2016-12-26 23:18:31 -0500

    turns out the example works

--- index.js
+++ index.js
@@ -10,12 +10,47 @@ let controller = Botkit.facebookbot({
   validate_requests: true,
 });
 
-let bot = controller.spawn({});
-
-controller.setupWebserver(process.env.PORT, function(err, webserver) {
-  controller.createWebhookEndpoints(webserver, bot, function() {
-    console.log('ONLINE!');
-  });
+var bot = controller.spawn({
 });
 
+controller.setupWebserver(process.env.PORT, function(err, webserver) {
+    controller.createWebhookEndpoints(webserver, bot, function() {
+        console.log('ONLINE!');
+    });
+});
+
+controller.api.thread_settings.greeting('Hello! I\'m a Botkit bot!');
+controller.api.thread_settings.get_started('sample_get_started_payload');
+controller.api.thread_settings.menu([
+    {
+        "type":"postback",
+        "title":"Hello",
+        "payload":"hello"
+    },
+    {
+        "type":"postback",
+        "title":"Help",
+        "payload":"help"
+    },
+    {
+      "type":"web_url",
+      "title":"Botkit Docs",
+      "url":"https://github.com/howdyai/botkit/blob/master/readme-facebook.md"
+    },
+]);
+
+controller.hears(['^hello', '^hi'], 'message_received,facebook_postback', function(bot, message) {
+    controller.storage.users.get(message.user, function(err, user) {
+        if (user && user.name) {
+            bot.reply(message, 'Hello ' + user.name + '!!');
+        } else {
+            bot.reply(message, 'Hello.');
+        }
+    });
+});
+
+controller.on('message_received', function(bot, message) {
+    bot.reply(message, 'idk wut that means');
+    return false;
+});
 
