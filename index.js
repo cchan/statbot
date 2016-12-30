eaedec61d272d5cb084622dbe9e0f7c53ad2f21e
Clive Chan <cc@clive.io>
2016-12-30 02:48:53 -0500

    gradually generalizing; experimentally figuring out botkit because their docs are bad

--- index.js
+++ index.js
@@ -2,6 +2,28 @@ require('dotenv-safe').load();
 let Botkit = require('botkit');
 let pm2 = require('pm2');
 
+/*
+let statbot = require('statbot')({
+  //Just pass in the botkit controller?
+  //IS THERE A SUCH THING AS BOTKIT MIDDLEWARES? IF SO THEN I NEED THAT.
+});
+
+
+//In statbot.use(statbot.logwatch({options})):
+statbot.says(say => {
+  pm2.launchBus((err, bus) => {
+    bus.on('log:out', data => {
+      say(data);
+    });
+  });
+});
+
+statbot.hears(["status"], reply => {
+  reply(blah);
+});
+*/
+
+
 let controller = Botkit.facebookbot({
   debug: true,
   log: true,
@@ -11,51 +33,52 @@ let controller = Botkit.facebookbot({
   validate_requests: true,
 });
 
-var bot = controller.spawn({
-});
+var bot = controller.spawn({});
+
+
+
+module.exports.says = function(callback){
+  function say(thing){
+    bot.say({
+      text: JSON.stringify(thing),
+      channel: process.env.FB_USER_ID
+    });
+  }
+  
+  callback(say);
+};
+
+
+
+module.exports.hears(matches, callback){
+  controller.hears(matches, 'message_received,facebook_postback', function(bot, message){
+    function reply(text){
+      bot.reply(message, text);
+    }
+    callback(reply);
+  });
+}
 
 controller.setupWebserver(process.env.PORT, function(err, webserver) {
   controller.createWebhookEndpoints(webserver, bot, function() {
-    pm2.launchBus((err, bus) => {
-      bus.on('log:out', data => {
-        //bot.reply...? bot.say?
-      });
-    });
+    console.log('Ready to receive messages');
   });
 });
 
-controller.api.thread_settings.greeting('Hello! I\'m a Botkit bot!');
-controller.api.thread_settings.get_started('sample_get_started_payload');
-controller.api.thread_settings.menu([
-    {
-        "type":"postback",
-        "title":"Hello",
-        "payload":"hello"
-    },
-    {
-        "type":"postback",
-        "title":"Help",
-        "payload":"help"
-    },
-    {
-      "type":"web_url",
-      "title":"Botkit Docs",
-      "url":"https://github.com/howdyai/botkit/blob/master/readme-facebook.md"
-    },
-]);
+
 
 controller.hears(['^hello', '^hi'], 'message_received,facebook_postback', function(bot, message) {
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Hello ' + user.name + '!!');
-        } else {
-            bot.reply(message, 'Hello.');
-        }
-    });
+  bot.reply(message, JSON.stringify(message.user));
+  controller.storage.users.get(message.user, function(err, user) {
+    if (user && user.name)
+      bot.reply(message, 'Hello ' + user.name + '!!');
+    else
+      bot.reply(message, 'Hello.' + JSON.stringify(user));
+  });
 });
+
 
 controller.on('message_received', function(bot, message) {
-    bot.reply(message, 'idk wut that means');
+    bot.reply(message, 'Unknown command.');
     return false;
 });
-
