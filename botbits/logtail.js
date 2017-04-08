a5b816a3d998f6e7b80a8f98482de6a7395ca67b
Clive Chan <cc@clive.io>
2017-04-08 19:14:46 -0400

    logtail bug

--- botbits/logtail.js
+++ botbits/logtail.js
@@ -1,9 +1,9 @@
-module.exports = function(options){ // For example, statbot.use(logtail({file:"/var/log/secure"}))
+module.exports = function(file, options){ // For example, statbot.use(logtail("/var/log/secure"))
   let Tail = require('tail').Tail;
-  let tailer = new Tail(options.file, {follow: true});
+  let tailer = new Tail(file, {follow: true});
   
   return function(say){
-    tailer.on('line', (data) => say(options.file, data));
-    tailer.on('err', (data) => say(options.file + ' err', data));
+    tailer.on('line', (data) => say(file, data));
+    tailer.on('err', (data) => say(file + ' err', data));
   }
 };
